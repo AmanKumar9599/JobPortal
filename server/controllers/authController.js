104f02d81cf19df045dc90e5ac1817704e7630a1
Aman Kumar <amankuntal9599@gmail.com>
2025-08-18 16:31:45 +0530

    Token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -120,6 +120,8 @@ const loginUser = async (req, res) => {
 
    res.cookie('token', token, {
   httpOnly: true,
+  secure: true,       
+  sameSite: "None",
   path: '/',
   maxAge: 24 * 60 * 60 * 1000,
 });
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://job-portal-beryl-nu.vercel.app'],
   credentials: true
 }));
 
